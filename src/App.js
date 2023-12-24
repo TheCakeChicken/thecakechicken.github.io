3a545c6dcb2a46b27327ba3cd4422d921bd9c325
Alyx <cake@cakechicken.me>
2023-12-24 21:59:24 +0000

    good website innit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="bg-slate-900 h-full flex justify-center items-center gap-5">
+            <img src="https://github.com/TheCakeChicken.png" alt="Alyx's GitHub avatar" className="rounded-full w-32 h-32" />
+
+            <div className="text-center text-white">
+                <h1 className="font-bold text-4xl">Hi!</h1>
+                <h2 className="text-xl">I'm Alyx</h2>
+            </div>
+        </div>
+    );
 }
 
 export default App;
